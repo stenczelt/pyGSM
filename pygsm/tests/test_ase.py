7375de7142ce73fa5a88b8574f1795e0d8d6764e
T. K. Stenczel <tks32@cam.ac.uk>
2021-05-29 12:56:43 +0100

    TST: add test one more test for ase stuff

--- pygsm/tests/test_ase.py
+++ pygsm/tests/test_ase.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pytest import approx
 
-from pygsm.level_of_theories.ase import geom_to_ase
+from pygsm.level_of_theories.ase import geom_to_ase, xyz_to_ase
 
 
 def test_geom_to_ase():
@@ -12,3 +12,16 @@ def test_geom_to_ase():
 
     assert atoms.get_chemical_symbols() == ["H", "He", "Li", "Be"]
     assert atoms.get_positions() == approx(xyz)
+
+
+def test_xyz_to_ase():
+    xyz_4x4 = [
+        ["H", 1., 2., 3.],
+        ["He", 11., 12., 13.],
+        ["Li", 21., 32., 43.],
+        ["Be", 31., 32., 33.],
+    ]
+
+    atoms = xyz_to_ase(xyz_4x4)
+    assert atoms.get_chemical_symbols() == ["H", "He", "Li", "Be"]
+    assert atoms.get_positions() == approx(np.array([x[1:] for x in xyz_4x4]))
